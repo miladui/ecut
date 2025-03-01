9ba273d168ad73cc355a7bcec707dcdf6559f8a9
milad <miladrealkord@gmail.com>
2025-03-01 13:31:39 +0330

    checkout desktop

--- checkout.php
+++ checkout.php
@@ -4,15 +4,118 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<section class="s-checkout max-[900px]:px-[12px] mt-[7rem]">
+<section class="s-checkout max-[900px]:px-[12px] mb-[2rem] mt-[7rem]">
     <div class="c-container">
-        <div class="p-checkout max-[900px]:mb-[2rem] gap-[1.5rem] flex max-[900px]:flex-col items-start justify-between mx-auto">
-            <div class="start gap-[1rem] flex flex-col items-center w-[57%]">
-                <div class="box-basket w-full border-[#263238] border-[1px] rounded-[8px] h-[550px]"></div>
-                <div class="box-address w-full border-[#263238] border-[1px] rounded-[8px] h-[250px]"></div>
+        <div class="p-checkout max-[900px]:mb-[2rem] gap-[1.5rem]  flex max-[900px]:flex-col items-start justify-between mx-auto">
+            <div class="start max-[900px]:w-full gap-[1rem] flex flex-col items-center w-[57%]">
+                <div class="box-basket w-full border-[#d4d4d4] border-[1px] rounded-[8px] h-[548px] px-[1.5rem] py-[2rem] flex flex-col justify-between items-center">
+                    <div class="title-basket flex items-center gap-[1rem]">
+                        <h4 class="font-bold mb-0 text-[15px]">بازبینی سبد خرید کالا</h4>
+                        <span class="bg-base-color-two relative top-[-1px] rounded-[5px] text-white text-[13px] py-[1px] px-[5px]">3 کالا</span>
+                    </div>
+                    <div class="items-basket w-full flex justify-center items-center gap-[14px]">
+                        <div class="cart-item flex flex-col items-center w-[19.5%]">
+                            <div class="title-cart h-[40px] bg-[#d8d8d8] flex items-center justify-center  text-[14px] w-full">
+                                پلو شرت
+                            </div>
+                            <div class="p-img w-full overflow-hidden mt-[7px] relative">
+                                <img src="assets/images/d1.jpg" alt="">
+                                <div class="quantity w-[23px] h-[23px] rounded-[50%] flex items-center justify-center bg-base-color-two text-[#fff] text-[13px] absolute right-[6px] top-[6px]">
+                                    3
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cart-item flex flex-col items-center w-[19.5%]">
+                            <div class="title-cart h-[40px] bg-[#d8d8d8] flex items-center justify-center  text-[14px] w-full">
+                                پلو شرت
+                            </div>
+                            <div class="p-img w-full overflow-hidden mt-[7px] relative">
+                                <img src="assets/images/d1.jpg" alt="">
+                                <div class="quantity w-[23px] h-[23px] rounded-[50%] flex items-center justify-center bg-base-color-two text-[#fff] text-[13px] absolute right-[6px] top-[6px]">
+                                    3
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cart-item flex flex-col items-center w-[19.5%]">
+                            <div class="title-cart h-[40px] bg-[#d8d8d8] flex items-center justify-center  text-[14px] w-full">
+                                پلو شرت
+                            </div>
+                            <div class="p-img w-full overflow-hidden mt-[7px] relative">
+                                <img src="assets/images/d1.jpg" alt="">
+                                <div class="quantity w-[23px] h-[23px] rounded-[50%] flex items-center justify-center bg-base-color-two text-[#fff] text-[13px] absolute right-[6px] top-[6px]">
+                                    3
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cart-item flex flex-col items-center w-[19.5%]">
+                            <div class="title-cart h-[40px] bg-[#d8d8d8] flex items-center justify-center  text-[14px] w-full">
+                                پلو شرت
+                            </div>
+                            <div class="p-img w-full overflow-hidden mt-[7px] relative">
+                                <img src="assets/images/d1.jpg" alt="">
+                                <div class="quantity w-[23px] h-[23px] rounded-[50%] flex items-center justify-center bg-base-color-two text-[#fff] text-[13px] absolute right-[6px] top-[6px]">
+                                    3
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cart-item flex flex-col items-center w-[19.5%]">
+                            <div class="title-cart h-[40px] bg-[#d8d8d8] flex items-center justify-center  text-[14px] w-full">
+                                پلو شرت
+                            </div>
+                            <div class="p-img w-full overflow-hidden mt-[7px] relative">
+                                <img src="assets/images/d1.jpg" alt="">
+                                <div class="quantity w-[23px] h-[23px] rounded-[50%] flex items-center justify-center bg-base-color-two text-[#fff] text-[13px] absolute right-[6px] top-[6px]">
+                                    3
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full flex flex-col items-center">
+                        <span class="text-[#777777] max-[900px]:text-[13px] max-[900px]:text-center text-[14px]">با کلیک بر روی هر آیتم ،تعداد و جزییات آن به تفکیک قابل مشاهده است</span>
+                        <button class="btn-base-two px-[1.5rem] text-[14px] h-[50px] mt-[1.5rem]">مشاهده سبد خرید
+                        </button>
+
+                    </div>
+                </div>
+                <div class="box-address max-[900px]:h-auto  w-full border-[#d4d4d4] border-[1px] rounded-[8px] h-[225px] flex items-center justify-between p-[1.6rem] max-[900px]:p-[1rem] max-[900px]:flex-col">
+                    <div class="detail-address w-[58%] max-[900px]:w-full flex flex-col items-start">
+                        <span class="text-[14px] mb-[1rem] font-bold">آدرس انتخاب شده</span>
+                        <span class="text-[#777777] text-[14px]">تهران، منطقه 3</span>
+                        <span class="text-[#777777] text-[14px]"> میرداماد، نبش کازرون جنوبی، ساختمان ایکات، پلاک 202</span>
+                        <div class="flex items-center justify-start">
+                            <span class="text-[#777777] text-[14px]">تحویل گیرنده :</span>
+                            <span class="text-[#777777] text-[14px]">مهدی بهشتی</span>
+                        </div>
+                        <div class="flex items-center justify-start">
+                            <span class="text-[#777777] text-[14px]">تلفن :</span>
+                            <span class="text-[#777777] text-[14px]">09121266804</span>
+                        </div>
+                        <button class="btn-base-two px-[1.3rem] h-[45px] text-[13px] mt-[1rem]">تغییر یا ویرایش آدرس
+                        </button>
+                    </div>
+                    <div class="shipping-method max-[900px]:mt-[1.5rem] w-[37%] max-[900px]:w-full flex flex-col items-center">
+                        <div onclick="changeSelectShipping('one')" class="item-shipping cursor-pointer w-full h-[70px] rounded-[7px] bg-white border-[1px] border-[#d4d4d4] flex items-center justify-start px-[1rem]">
+                                <div class="radio-button bg-white w-[20px] h-[20px] border-[1px] border-[#d4d4d4] rounded-[50%] flex items-center justify-center p-[4px]">
+                                    <div class="fill bg-white w-full h-full rounded-[50%]"></div>
+
+                                </div>
+                                <div class="img rounded-[4px] mx-[13px] overflow-hidden bg-base-color-two w-[45px] h-[45px]"></div>
+                             <span class="text-[#777777] text-[14px]">ارسال با پست پیشتاز</span>
+                        </div>
+                        <div onclick="changeSelectShipping('two')" class="item-shipping cursor-pointer mt-[1.5rem] w-full h-[70px] rounded-[7px] bg-white border-[1px] border-[#d4d4d4] flex items-center justify-start px-[1rem]">
+                            <div class="radio-button  bg-white w-[20px] h-[20px] border-[1px] border-[#d4d4d4] rounded-[50%] flex items-center justify-center p-[4px]">
+                                <div class="fill bg-white w-full h-full rounded-[50%]"></div>
+
+                            </div>
+                            <div class="img rounded-[4px] mx-[13px] overflow-hidden bg-base-color-two w-[45px] h-[45px]"></div>
+                            <span class="text-[#777777] text-[14px]">ارسال توسط پیک</span>
+                        </div>
+
+                    </div>
+                </div>
 
             </div>
-            <div class="end w-[43%]  bg-[#f2f2f2] rounded-[8px] px-[3.2rem] py-[1.2rem]">
+            <div class="end max-[900px]:w-full w-[43%]  bg-[#f2f2f2] rounded-[8px] px-[3.2rem] py-[2rem]">
                 <h3 class="font-bold md:text-[15px] mb-[1rem] xl:text-[16px]">جمع سبد خرید شما</h3>
                 <div class="item-simple w-full flex items-center mt-[12px] justify-between">
                     <span class="font-bold text-[14px]">مبلغ محصولات :</span>
@@ -208,6 +311,10 @@
 
     function changeSelectPayment(activeItem) {
         $('.choose-payment button').removeClass('active');
+        $(event.target).addClass('active');
+    }
+    function changeSelectShipping(activeItemTwo) {
+        $('.shipping-method .item-shipping').removeClass('active');
         $(event.target).addClass('active');
     }
 </script>
